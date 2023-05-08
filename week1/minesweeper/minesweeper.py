10eac5fc1f4c3375c790e09d5dbced4cca370f85
pudimlucy <dourolucy@gmail.com>
2023-05-08 11:34:11 -0300

    --- Minesweeper 1.0 --- 1. know_mines and know_safes functions implemented 2. mark_mine and mark_safe functions implemented 3. progress on add_knowledge function

--- week1/minesweeper/minesweeper.py
+++ week1/minesweeper/minesweeper.py
@@ -105,27 +105,30 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        return self.cells if len(self.cells) == self.count else set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        return self.cells if len(self.cells) == 0 else set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
@@ -182,7 +185,25 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
+
+        cells = set()
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (cell[0]+i, cell[1]+j) not in self.mines and (cell[0]+i, cell[1]+j) not in self.safes:
+                    cells.add((cell[0]+i, cell[1]+j))
+        self.knowledge.add(Sentence(cells, count))
+
+        while True:
+            for sentence in self.knowledge:
+                for safe in sentence.know_safes():
+                    self.mark_safe(safe)
+                for mine in sentence.know_mines():
+                    self.mark_safe(cell)
+
+
 
     def make_safe_move(self):
         """
